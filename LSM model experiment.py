2020c7e4a30757deae2487c05f8d047d0d188601
fredmohansun <mohan.sun@aggiemail.usu.edu>
2016-11-27 00:06:36 -0700

    LSM v0.3

--- LSM model experiment.py
+++ LSM model experiment.py
@@ -16,19 +16,17 @@ S0 = 41
 sigma = .3
 r = .08
 div = 0
-N = 3
-M = 10000
+N = 4
+M = 100000
 
 ## Calculating Parameters
 h = T/N
 nuh = (r-div-0.5*sigma*sigma)*h
 sigsh = np.sqrt(h) * sigma
-np.random.seed(1234)
 
 ## X is for log(S)
 X0 = np.log(S0)
 XM = np.zeros((M,N+1))
-
 XM[:,0] = X0
 
 ## Calculate X for N steps for M simulation, and get S matrix by exp(X)
@@ -46,16 +44,14 @@ CF[:,N+1] = np.maximum(K-S[:,N],0)
 for i in range(M):
     CF[i][0]=i
 
+
 ## Recursion part on calculating CF
-for i in range(N-1,0,-1):
-    
+for i in range(N-1,0,-1):    
 ## Stemp is the column drawn from S, indStemp is used to check if option is in the money
     Stemp = S[:,i]
-    indStemp = Stemp < K
-    
+    ind = Stemp < K    
 ## Numtemp contains the row index of each dataset
-    Numtemp = CF[:,0]
-    
+    Numtemp = CF[:,0]    
 ## CFtemp is the PV of continuation value at the time horizon t = i
     ncol = N-i
     CFtemp1 = np.zeros((M,ncol))
@@ -64,26 +60,21 @@ for i in range(N-1,0,-1):
         
     CFtemp = np.zeros(M)
         
-    for k in range(ncol):
-        CFtemp += CFtemp1[:,k] * np.exp(-r*h*(k+1))
+    for j in range(ncol):
+        CFtemp += CFtemp1[:,j] * np.exp(-r*h*(j+1))
     
-
-    size = len(Stemp[indStemp])
-
+    size = len(Stemp[ind])
 ## Linear Regression
-
     X = np.ones((size,3))
-    X[:,0] = Stemp[indStemp]
-    X[:,1] = Stemp[indStemp] ** 2
-    Y = CFtemp[indStemp]
+    X[:,0] = Stemp[ind]
+    X[:,1] = Stemp[ind] * Stemp[ind]
+    Y = CFtemp[ind]
     
     beta1, beta2, beta0 = np.linalg.lstsq(X,Y)[0]
-
 ## Calculate predict PV of Contiunation value and get the payoff at t = i
     CVhat = beta0 + beta1 * X[:,0] + beta2 * X[:,1]    
-    Payofftemp = np.maximum(K-X[:,0],0)    
-    Num = Numtemp[indStemp]
-    
+    Payofftemp = np.maximum(K-X[:,0],0)
+    Num = Numtemp[ind]    
 ## if the PV of continuation value is less than payoff at t = i, individual
 ## choose to exercise early and get the payoff
     for j in range(size):
@@ -92,17 +83,14 @@ for i in range(N-1,0,-1):
                 CF[Num[j]][i+2] = 0.0
                 
 ## End of recursion
-
-Sum = 0.0    
-
+                
+                
+                
 ## the value of option for each simulation is the sum of all PV of continuation value
 for i in range(2,N+2):
     CF[:,1] += CF[:,i] * np.exp(-r*h*(i-1))
     
 Sum = np.sum(CF[:,1])
-
-## price is the average of all simulation
 price = Sum/M
 
-print(CF)
 print(price)
